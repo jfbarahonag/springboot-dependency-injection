c3733eb8f1bb0353c0e59d81b94cb5fcd5073e5c
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-13 19:56:27 -0500

    the product repository has been created

---
+++ src/main/java/com/jfbarahonag/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,30 @@
+package com.jfbarahonag.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.jfbarahonag.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+  private List<Product> data;
+
+  public ProductRepository() {
+    data = Arrays.asList(
+        // default values
+        new Product(1L, "Vela x10gr", 5500L),
+        new Product(2L, "Caja de regalo", 1000L),
+        new Product(3L, "Dulces", 200L));
+  }
+
+  public List<Product> findAll() {
+    return data;
+  }
+
+  public Product findById(Long id) {
+    return data.stream()
+        .filter(product -> product.getId().equals(id))
+        .findFirst()
+        .orElse(null);
+  }
+
+}
