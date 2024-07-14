edbf6c6aaa814d91c4bffdf9c4d94c0ab6a53e4e
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-14 15:02:19 -0500

    new repository implementation has been added to get the data from a JSON file

---
+++ src/main/java/com/jfbarahonag/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,47 @@
+package com.jfbarahonag.di.app.springboot_di.repositories;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jfbarahonag.di.app.springboot_di.models.Product;
+
+@Repository
+public class ProductRepositoryJson implements ProductRepository {
+
+  private List<Product> data;
+
+  public ProductRepositoryJson() {
+    loadData();
+  }
+
+  private void loadData() {
+    ObjectMapper mapper = new ObjectMapper();
+    TypeReference<List<Product>> typeReference = new TypeReference<List<Product>>() {
+    };
+    InputStream inputStream = getClass().getResourceAsStream("/products.json");
+    try {
+      this.data = mapper.readValue(inputStream, typeReference);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to load products from JSON", e);
+    }
+  }
+
+  @Override
+  public List<Product> findAll() {
+    return data;
+  }
+
+  @Override
+  public Product findById(Long id) {
+    return data.stream()
+        .filter(product -> product.getId().equals(id))
+        .findFirst()
+        .orElse(null);
+  }
+
+}
