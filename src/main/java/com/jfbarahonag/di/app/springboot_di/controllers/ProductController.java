c4d423f1c352d40c36e3f1d06ef8ec543f57c34f
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-13 20:26:48 -0500

    Product controller has been added

---
+++ src/main/java/com/jfbarahonag/di/app/springboot_di/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.jfbarahonag.di.app.springboot_di.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jfbarahonag.di.app.springboot_di.models.Product;
+import com.jfbarahonag.di.app.springboot_di.services.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+  private ProductService productService = new ProductService();
+
+  @GetMapping
+  public List<Product> list() {
+    return productService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Product getProduct(@PathVariable Long id) {
+    return productService.findById(id);
+  }
+
+}
