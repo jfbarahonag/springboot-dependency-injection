b1d76bbdba6e2f4723631380733661931cbf3bfd
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-16 21:20:28 -0500

    editing the way to load JSON data

--- src/main/java/com/jfbarahonag/di/app/springboot_di/repositories/ProductRepositoryJson.java
+++ src/main/java/com/jfbarahonag/di/app/springboot_di/repositories/ProductRepositoryJson.java
@@ -1,12 +1,13 @@
 package com.jfbarahonag.di.app.springboot_di.repositories;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jfbarahonag.di.app.springboot_di.models.Product;
 
@@ -20,12 +21,14 @@ public class ProductRepositoryJson implements ProductRepository {
   }
 
   private void loadData() {
+    Resource resource = new ClassPathResource("/json/products.json");
     ObjectMapper mapper = new ObjectMapper();
-    TypeReference<List<Product>> typeReference = new TypeReference<List<Product>>() {
-    };
-    InputStream inputStream = getClass().getResourceAsStream("/products.json");
+    
     try {
-      this.data = mapper.readValue(inputStream, typeReference);
+      data = Arrays.asList(
+          mapper.readValue(
+              resource.getFile(),
+              Product[].class));
     } catch (IOException e) {
       throw new RuntimeException("Unable to load products from JSON", e);
     }
